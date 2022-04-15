809244a7409fbf1e379b9e73bfdbd1afd757d901
khl6235 <khl6235@naver.com>
2022-04-15 16:37:19 +0900

    initial commit

---
+++ src/main/kotlin/com/kopring/haely/HaelyApplication.kt
@@ -0,0 +1,11 @@
+package com.kopring.haely
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HaelyApplication
+
+fun main(args: Array<String>) {
+	runApplication<HaelyApplication>(*args)
+}
---
+++ src/test/kotlin/com/kopring/haely/HaelyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kopring.haely
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HaelyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
